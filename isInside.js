2c740215b447c9678112be61ab2fa98b37583b34
Peter Langley <plangley@nd27498.ordsvy.gov.uk>
2014-08-18 10:56:50 +0100

    Added a falsy check on the parent

--- isInside.js
+++ isInside.js
@@ -1,30 +1,36 @@
 document.body.addEventListener( 'click', bodyOnClick, false );
 
 function bodyOnClick( event ) {
-	//if( ! isInside( 'id', 'js-box', event ) )
-	//if( ! isInside( 'data-id', 'box', event ) )
-	if( ! isInside( 'class', 'box', event ) )
-		console.log( 'not inside' );
+  //if( ! isInside( 'id', 'js-box', event ) )
+  //if( ! isInside( 'data-id', 'box', event ) )
+  if( ! isInside( 'class', 'box', event ) ) {
+    console.log( 'not inside' );
+  }
 }
 
-/*
+/**
  * Check whether a click/tap event occured inside of an element
  */
 function isInside( attribute, value, event ) {
-	if( event.target.getAttribute( attribute ) == value ) {
-		return true;
-	}
+  if( event.target.getAttribute( attribute ) == value ) {
+    return true;
+  }
 
-	var parent = event.target.parentNode;
+  var parent = event.target.parentNode;
 
-	while( parent.getAttribute( attribute ) != value ) {
-		if( parent.tagName.toLowerCase() == 'html' ) {
-			return false;
-			break;
-		}
-		parent = parent.parentNode;
-	}
+  while( parent.getAttribute( attribute ) != value ) {
+    if( parent.tagName.toLowerCase() == 'html' ) {
+      return false;
+      break;
+    }
+    parent = parent.parentNode;
 
-	// Correct parent found - proving we are inside
-	return true;
+    if( ! parent ) {
+      return false;
+      break;
+    }
+  }
+
+  // Correct parent found - proving we are inside
+  return true;
 }
